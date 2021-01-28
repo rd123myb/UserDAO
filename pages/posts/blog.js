ccf88ba9fa1db9079091f6213c9e1f4158fde422
Rory <roryjd123@gmail.com>
2021-01-29 12:34:09 +1300

    grammer

--- pages/posts/blog.js
+++ pages/posts/blog.js
@@ -8,7 +8,7 @@ export default function FirstPost() {
       
         
           <p> UserDAO gives users the tools they need to generate structured data from the opinions they have about products.
-              This data is highly valued bycompanies during in thier development process and help them achieve good market fit.
+              This data is highly valuable by companies who use it to achieve good product market fit.
               Currently, this type of high quality data is gathered mostly by either user testing, focus groups or surveys that don't incentivise consumers adequatly. 
               Furthermore, most of this data is siloed within 
               the companies that pay to generate it, leaving countless potential for innovation unrealised at the expense of 
@@ -16,14 +16,15 @@ export default function FirstPost() {
           </p>
 
           <p> UserDAO is a decentralized and open-source system for meeting the demand for this data in an open marketplace.
-              Specialised surveys and analysis tools can be developed to turn your opinions and preferences about every aspect of
-               a given product into the type of valuable data that companies need in a competitive consumer market. When these opinions
+              Specialised surveys and analysis tools can to turn your opinions and preferences into the type of valuable data that companies need in a competitive consumer market. When these opinions
                are captured and structured at scale, thier value is immense. UserDAO creates this effect and redistrubtes revenue back to 
                contributiors via smart contracts.
+
+            </p>
                
+            <h4>It's obvious that some products are better than others, the question is WHY.</h4>
                
-               
-           </p>
+           
            <h3> More information and progress coming soon</h3>
            {/* <p>It's obvious that some products are better than others, but the real question is WHY.</p>
            <p>
