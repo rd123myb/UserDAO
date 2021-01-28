24c4afa88edf5c8488ce4ad443bc1b94bb6143e7
Rory <roryjd123@gmail.com>
2021-01-29 12:26:33 +1300

    intro update

--- pages/posts/blog.js
+++ pages/posts/blog.js
@@ -4,18 +4,19 @@ import Layout from '../../components/layout'
 export default function FirstPost() {
   return (
     <Layout>
-      <h1>So why let it be priced at zero?</h1>
+      <h1>So why price at zero?</h1>
       
         
-          <p> UserDAO is a DataDao usecase that gives users the tools they need to generate structured data from the opinions they have about products.
-              This data is valuable to companies who use it in thier development process to achieve market fit.
-              Currently, this type of high quality data is gathered mostly by either user testing, focus groups or surveys that don't incentivise consumers adequatly. Furthermore, most of this data is siloed within 
+          <p> UserDAO gives users the tools they need to generate structured data from the opinions they have about products.
+              This data is highly valued bycompanies during in thier development process and help them achieve good market fit.
+              Currently, this type of high quality data is gathered mostly by either user testing, focus groups or surveys that don't incentivise consumers adequatly. 
+              Furthermore, most of this data is siloed within 
               the companies that pay to generate it, leaving countless potential for innovation unrealised at the expense of 
                consumers around the world.
           </p>
 
-          <p> UserDAO is a decentralized and open-source system for meeting this demand in an open marketplace.
-              Specialised surveys and analysis tools are desinged to turn your thoughts and preferences about every aspect of
+          <p> UserDAO is a decentralized and open-source system for meeting the demand for this data in an open marketplace.
+              Specialised surveys and analysis tools can be developed to turn your opinions and preferences about every aspect of
                a given product into the type of valuable data that companies need in a competitive consumer market. When these opinions
                are captured and structured at scale, thier value is immense. UserDAO creates this effect and redistrubtes revenue back to 
                contributiors via smart contracts.
@@ -23,14 +24,15 @@ export default function FirstPost() {
                
                
            </p>
-           <p>It's obvious that some products are better than others, but the million dollar question is WHY.</p>
+           <h3> More information and progress coming soon</h3>
+           {/* <p>It's obvious that some products are better than others, but the real question is WHY.</p>
            <p>
              Through the advent of Blockchain powered data markets, DAO's, and Decentralized finance,
                everyday consumers now have a direct market to take thier opinions, all while helping smaller companies that would otherwise
                not have the resources to compete with larger companies in the data driven economy.
 
                
-           </p>
+           </p> */}
 
       
     </Layout>
