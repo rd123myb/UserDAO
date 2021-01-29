bae09a1c4f3f059ae470c5b4e789e487f35fc82a
Rory <roryjd123@gmail.com>
2021-01-29 13:33:22 +1300

    shorten intro

--- pages/posts/blog.js
+++ pages/posts/blog.js
@@ -10,13 +10,14 @@ export default function FirstPost() {
           <p> UserDAO gives users the tools they need to generate structured data from thier opinions on products.
               This data is highly valuable to companies who use it to achieve good product market fit.
               Currently, this type of high quality data is gathered mostly by either user testing, focus groups or surveys that don't incentivise consumers adequatly. 
-              Furthermore, most of this data is siloed within 
+              
+              </p> <p>Furthermore, most of this data is siloed within 
               the companies that pay to generate it, leaving countless potential for innovation unrealised at the expense of 
                consumers around the world.
           </p>
 
           <p> UserDAO is a decentralized and open-source system for meeting the demand for this data in an open marketplace.
-              Specialised surveys and analysis tools can to turn your opinions and preferences into the type of valuable data that companies need in a competitive consumer market. When these opinions
+              Specialised analysis tools can to turn your opinions into the type of data that companies want. When these opinions
                are captured and structured at scale, thier value is immense. UserDAO creates this effect and redistrubtes revenue back to 
                contributiors via smart contracts.
 
