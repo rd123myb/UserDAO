e9aa16736cd3ff26f73799a9e6769e7ba4bfbb6c
Rory <roryjd123@gmail.com>
2021-01-30 00:19:26 +1300

    outline steps

--- pages/posts/blog.js
+++ pages/posts/blog.js
@@ -4,7 +4,7 @@ import Layout from '../../components/layout'
 export default function FirstPost() {
   return (
     <Layout>
-     <h4>Some products are better than others, the question is why?</h4>
+     {/* <h4>Some products are better than others, the question is why?</h4> */}
     
       
     <p> UserDAO is a decentralised and open-source system that gives users the tools to structure their opinions into a dataset.
@@ -17,9 +17,19 @@ export default function FirstPost() {
          consumers.
     </p>
 
-    <p> The demand for this data can now be met in an open marketplace.
-        Specialised analysis tools capture and structure at members opinions at scale, and monitise data, and redistribute revenue back to
-         contributors via smart contracts.
+    <h3> The demand for this data can now be met in an open marketplace.</h3>
+
+    <p>
+        1)Specialised analysis tools capture and structure members opinions for specific products.
+        </p><p>
+        2)Opinions are pool into a dataset along side objective datapoints relating to the product.
+        </p><p>
+        3)Dataset is published on Ocean market to monitise data.
+        </p><p>
+        4)Revenue gained from the sale of the data is removed and distributed to DAO members via smart contracts.
+        </p><p>
+        5)Elected members fill in gaps where needed.
+
 
             </p>
                
