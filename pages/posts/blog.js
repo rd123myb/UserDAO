51bb4a941c0fcaa78b92873cb250f0b542f1f40a
Rory <roryjd123@gmail.com>
2021-01-29 13:37:58 +1300

    blog shorten

--- pages/posts/blog.js
+++ pages/posts/blog.js
@@ -7,17 +7,18 @@ export default function FirstPost() {
       <h1>So why price at zero?</h1>
       
         
-          <p> UserDAO gives users the tools they need to generate structured data from thier opinions on products.
-              This data is highly valuable to companies who use it to achieve good product market fit.
-              Currently, this type of high quality data is gathered mostly by either user testing, focus groups or surveys that don't incentivise consumers adequatly. 
+          <p> UserDAO is a decentralized and open-source system that gives users the tools structure thier opinions on products into a dataset.
+              This data is highly valuable to companies who use it to achieve good product market fit, yet is currenty gathered by user testing, focus groups or surveys that don't incentivise consumers adequatly. 
               
-              </p> <p>Furthermore, most of this data is siloed within 
+              </p> 
+              
+              <p>Furthermore, most of this data is siloed within 
               the companies that pay to generate it, leaving countless potential for innovation unrealised at the expense of 
                consumers around the world.
           </p>
 
-          <p> UserDAO is a decentralized and open-source system for meeting the demand for this data in an open marketplace.
-              Specialised analysis tools can to turn your opinions into the type of data that companies want. When these opinions
+          <p> UserDAO members can meet the demand for this data in an open marketplace.
+              Specialised analysis tools can turn your opinions into the type of data that companies want. When these opinions
                are captured and structured at scale, thier value is immense. UserDAO creates this effect and redistrubtes revenue back to 
                contributiors via smart contracts.
 
