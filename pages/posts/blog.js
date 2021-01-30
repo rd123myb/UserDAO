25390f8d7493d751042319aaf591ab75b4702d4b
Rory <roryjd123@gmail.com>
2021-01-30 19:05:50 +1300

    spelling

--- pages/posts/blog.js
+++ pages/posts/blog.js
@@ -7,28 +7,28 @@ export default function FirstPost() {
      {/* <h4>Some products are better than others, the question is why?</h4> */}
     
       
-    <p> UserDAO is a decentralised and open-source system that gives users the tools to structure their opinions into a dataset.
-        This companies use this type of data to predict product market fit and identify emerging trends. Currently it is gathered by expensive user testing methods that don't reward consumers as they should.
+    <p> UserDAO gives users the tools to structure their opinions into a dataset.
+        This data can be used to predict product market fit and identify emerging trends. Currently it is gathered by expensive user testing methods that don't reward users as they should.
        
         </p>
        
-        <p>This data is valuable to both large companies and agile operations. Yet it is often generated and siloed within large companies due to inital cost. This centralises knowledge of how to meet consumer preferences, 
-          resulting in lesser quality products for consumers, and less opportunity for innovation and competion in the consumer market.
+        <p>This data is valuable to both large companies and agile operations. Yet it is often generated and siloed within large companies due to the cost of generating it. This centralises knowledge of how to meet consumer preferences, 
+          resulting in lesser quality products for consumers, and less opportunity for innovation and competion by smaller operations in the consumer market.
 
     </p>
 
     <h3> The demand for this data can now be met in an open marketplace.</h3>
 
     <p>
-        1)Specialised analysis tools capture and structure members opinions for specific products.
+        1) Specialised testing methods and analysis tools capture members opinions for specific products.
         </p><p>
-        2)Opinions are pool into a dataset along side objective datapoints relating to the product.
+        2) Opinions are transformed, structured and merged into a dataset that includes objective datapoints relating to the product.
         </p><p>
-        3)Dataset is published on Ocean market to monitise data.
+        3) Dataset is published on Ocean market to and sold for Ocean Tokens.
         </p><p>
-        4)Revenue gained from the sale of the data is removed and distributed to DAO members via smart contracts.
+        4) Revenue gained from sales is removed and distributed to DAO members via smart contracts.
         </p><p>
-        5)Elected members fill in gaps where needed.
+        5) Elected members fill in gaps where needed.
 
 
             </p>
