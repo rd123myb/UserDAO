8e5e8b95161a1e88740d2455bb1d0182c3c293fc
Rory <roryjd123@gmail.com>
2021-02-01 16:23:13 +1300

    build fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "48624d16e13ac64ddd23";
+/******/ 	var hotCurrentHash = "14e4d79282bbbcad84b4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
