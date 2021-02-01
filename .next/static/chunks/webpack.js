ea3ad59cd64e640cee2b48a222b06edb76926657
Rory <roryjd123@gmail.com>
2021-02-01 14:56:54 +1300

    surveyJS + table update draft

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0cf5dcabbfcb01df9ed2";
+/******/ 	var hotCurrentHash = "3f759aa6acb16715e3ae";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/general-surveys/first-survey.js
+++ pages/general-surveys/first-survey.js
@@ -1,15 +1,26 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../../components/layout";
+import React, {Component} from 'react';
+import * as Survey from 'survey-react';
+import 'survey-react/survey.css'
+import SignupForm from "../../components/surveys/signupForm"
+
 
 export default function FirstSurvey() {
+
+  
+
+
   return (
     <Layout>
       <Head>
         <title>First Survey</title>
       </Head>
       <h1>First Survey</h1>
-      <body></body>
+      <body>
+      
+      </body>
       <h2>
         <Link href="/">
           <a>Back to home</a>
