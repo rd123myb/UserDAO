debbe540c7974695fdc06c88c392b302b85f1854
Rory <roryjd123@gmail.com>
2021-02-02 14:25:09 +1300

    add login page react component

---
+++ pages/account/login.js
@@ -0,0 +1,40 @@
+
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+     {/* <h4>Some products are better than others, the question is why?</h4> */}
+    
+      
+    <p> UserDAO gives users the tools to structure their opinions into a dataset.
+        This data can be used to predict product market fit and identify emerging trends. Currently it is gathered by expensive user testing methods that don't reward users as they should.
+       
+        </p>
+       
+        <p>This data is valuable to both large companies and agile operations. Yet it is often generated and siloed within large companies due to the cost of generating it. This centralises knowledge of how to meet consumer preferences, 
+          resulting in lesser quality products for consumers. It also limits opportunity for smaller operations to compete successfully.
+
+    </p>
+
+    <h3> The demand for this data can now be met in an open marketplace.</h3>
+
+    <p>
+        1) Specialised testing methods and analysis tools capture members opinions for specific products.
+        </p><p>
+        2) Opinions are transformed, structured and merged into a dataset with objective datapoints relating to the product.
+        </p><p>
+        3) Dataset is published on Ocean market to and sold for OCEAN.
+        </p><p>
+        4) OCEAN gained from sales is removed from the pool and distributed to DAO members via smart contracts.
+        </p><p>
+        5) Elected members fill in gaps where needed.
+
+
+            </p>
+               
+            <h4></h4>
+            </Layout>
+  )
+}
+               
