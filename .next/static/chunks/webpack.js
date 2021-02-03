fe5a444a12f27b1807b1afb1f42226ccc39a0c1b
Rory <roryjd123@gmail.com>
2021-02-04 09:53:47 +1300

    country region panel

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f8f7b64a9c878efa5c07";
+/******/ 	var hotCurrentHash = "373a2782d5ba0e5331e2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/surveys/signupForm.js
+++ components/surveys/signupForm.js
@@ -1,73 +1,95 @@
-
-import 'survey-react/survey.css'
+import "survey-react/survey.css";
 
 // Json that is mapped into suvery on pages/signup.js
 
 export const json = {
-    questions: [
+  questions: [
+    {
+      name: "name",
+      type: "text",
+      title: "Please enter your name:",
+      placeHolder: "Satoshi Nakamoto",
+      isRequired: true,
+      autoComplete: "name",
+    },
+    {
+      name: "birthdate",
+      type: "text",
+      inputType: "date",
+      placeHolder: "15/09/2008",
+      title: "Your birthdate:",
+      isRequired: true,
+      autoComplete: "bdate",
+    },
+    {
+        name: "color",
+        type: "text",
+        inputType: "color",
+        isRequired: true,
+        title: "Your favorite color:"
+    },
+    {
+      name: "email",
+      type: "text",
+      inputType: "email",
+      title: "Your e-mail:",
+      placeHolder: "satoshi@gmail.com",
+      isRequired: true,
+      autoComplete: "email",
+      validators: [
         {
-            name: "name",
-            type: "text",
-            title: "Please enter your name:",
-            placeHolder: "Satoshi Nakamoto",
-            isRequired: true,
-            autoComplete: "name"
-        }, {
-            name: "birthdate",
-            type: "text",
-            inputType: "date",
-            placeHolder: "15/09/2008",
-            title: "Your birthdate:",
-            isRequired: true,
-            autoComplete: "bdate"
-        }, 
-        // {
-        //     name: "color",
-        //     type: "text",
-        //     inputType: "color",
-        //     isRequired: true,
-        //     title: "Your favorite color:"
-        // }, 
-        {
-            name: "email",
-            type: "text",
-            inputType: "email",
-            title: "Your e-mail:",
-            placeHolder: "satoshi@gmail.com",
-            isRequired: true,
-            autoComplete: "email",
-            validators: [
-                {
-                    type: "email"
+          type: "email",
+        },
+      ],
+    },
+    // {
+    //   type: "dropdown",
+    //   name: "country",
+    //   title: "Select the country...",
+    //   isRequired: true,
+    //   choicesByUrl: {
+    //     url: "https://restcountries.eu/rest/v2/all",
+    //     valueName: "name",
+    //   },
+    // },
+    {
+        type: "panel",
+        name: "panel_countriesByRegion",
+        title: "Show countries by region",
+        elements: [
+            {
+                type: "dropdown",
+                name: "region",
+                title: "Select the region...",
+                choices: ["Africa", "Americas", "Asia", "Europe", "Oceania"]
+            }, {
+                type: "dropdown",
+                name: "reg_country",
+                title: "Select the country...",
+                isRequired: true,
+                choicesByUrl: {
+                    url: "https://restcountries.eu/rest/v2/region/{region}",
+                    valueName: "name"
                 }
-            ]
-        },
-        {
-            type: "dropdown",
-            name: "country",
-            title: "Select the country...",
-            isRequired: true,
-            choicesByUrl: {
-                url: "https://restcountries.eu/rest/v2/all",
-                valueName: "name"
             }
-        },
-        {
-            name: "eth address",
-            type: "text",
-            title: "Your Ethereum Address",
-            isRequired: true,
-            placeHolder: "0x.....",
-            autoComplete: "eth"
-        }, 
-        {
-            name: "twitter",
-            type: "text",
-            title: "Twitter handle",
-            placeHolder: "@Satoshi2008",
-            isRequired: true,
-            placeHolder: "@Satoshi",
-            autoComplete: "eth"
-        }
-    ]
+        ]
+    },
+    {
+      name: "eth address",
+      type: "text",
+      title: "Your Ethereum Address",
+      isRequired: true,
+      placeHolder: "0x.....",
+      autoComplete: "eth",
+    },
+    {
+      name: "twitter",
+      type: "text",
+      title: "Twitter handle",
+      placeHolder: "@Satoshi2008",
+      isRequired: true,
+      placeHolder: "@Satoshi",
+      autoComplete: "eth",
+    },
+  ],
 };
